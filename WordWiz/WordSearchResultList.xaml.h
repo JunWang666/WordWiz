bfb0d3ad41f256f47e88a2d56f8f45c137a76a9f
WangJunwei233 <basket_blanker.2b@icloud.com>
2025-05-18 00:47:46 +0800

    update

--- WordWiz/WordSearchResultList.xaml.h
+++ WordWiz/WordSearchResultList.xaml.h
@@ -11,14 +11,8 @@ namespace winrt::WordWiz::implementation
 
         Windows::Foundation::Collections::IObservableVector<WordWiz::WordItem> Items();
 
-        void AddItemButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
         void AddSampleItems(); // From your original code
         void AddNewEntry(winrt::hstring const& word, winrt::hstring const& explanation); // From your original code, adapted
-
-        // Added from generic example
-        void RemoveSelectedItemsButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
-        void ClearListButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
-        void SelectionModeComboBox_SelectionChanged(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& args);
 
 
     private:
