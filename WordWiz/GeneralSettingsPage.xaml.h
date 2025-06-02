5caf86b3736c9cc03834fca3ffa5d5556e5a852e
WangJunwei233 <basket_blanker.2b@icloud.com>
2025-06-02 19:16:02 +0800

    增强文件路径管理和设置界面
    在 `FilePathProvider` 中添加了多个函数以获取应用的本地、缓存、设置、共享和临时文件夹路径，并实现了错误处理和日志记录功能。更新了 `GeneralSettingsPage` 的 UI，增加了显示和打开这些文件夹的按钮。修改了 `SettingsManager` 的构造和析构函数，使用 `std::unique_ptr` 管理数据库会话。更新项目文件以包含新的页面和模块。

--- WordWiz/GeneralSettingsPage.xaml.h
+++ WordWiz/GeneralSettingsPage.xaml.h
@@ -3,16 +3,20 @@
 #include "GeneralSettingsPage.g.h"
 
 namespace winrt::WordWiz::implementation
-{    struct GeneralSettingsPage : GeneralSettingsPageT<GeneralSettingsPage>
+{
+    struct GeneralSettingsPage : GeneralSettingsPageT<GeneralSettingsPage>
     {
         GeneralSettingsPage();
-
+        
         void ThemeRadio_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void OpenFolderButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
 
     private:
         void LoadCurrentTheme();
         void ApplyTheme(const std::string& themeMode);
         bool IsSystemInDarkMode();
+        void LoadFolderPaths();
+        void OpenFolderInExplorer(const std::string& folderPath);
     };
 }
 
